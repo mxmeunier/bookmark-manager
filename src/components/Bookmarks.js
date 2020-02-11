3a381da04152cd566db83733f65e7d3d5e834aa9
Maxime Meunier <mxmeunier@gmail.com>
2020-02-11 10:16:12 +0100

    use table for bookmarks

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -15,20 +15,43 @@ const Bookmarks = ({ bookmarks, remove }) => {
 
   return (
     <div>
-      <ul className="list-group">
-        {currentBookmarks.map((bookmark, index) => (
-          <li key={index} className="list-group-item">
-            <a href={bookmark.url}>{bookmark.title}</a>
-            {`, ${bookmark.author} , ${bookmark.addedDate.toLocaleString()} `}
-            <Link to={`/edit/${index}`}>
-              <button className="btn btn-light">Edit</button>
-            </Link>
-            <button className="btn btn-danger" onClick={() => remove(index)}>
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Added Date</th>
+            <th scope="col">#</th>
+            <th scope="col">#</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentBookmarks.map((bookmark, index) => (
+            <tr key={index}>
+              <td>
+                <img src={bookmark.url} alt={`flickr-${bookmark.title}`}></img>
+              </td>
+              <td>{bookmark.title}</td>
+              <td>{bookmark.author}</td>
+              <td>{bookmark.addedDate}</td>
+              <td>
+                <Link to={`/edit/${index}`}>
+                  <button className="btn btn-light">Edit</button>
+                </Link>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => remove(index)}
+                >
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Pagination
         bookmarksPerPage={BOOKMARKS_PER_PAGE}
         totalBookmarks={bookmarks.length}
